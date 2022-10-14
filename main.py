e16a788878f6b3afc75a2cff1cc2ea4e445d47e8
MaxVinneR <kostya.orehin@inbox.ru>
2022-10-14 17:05:42 +0400

    create  one_demential_array

---
+++ main.py
@@ -0,0 +1,23 @@
+
+print("размерность массива?")
+N = int(input())
+print(N)
+def one_dimensional_array():
+    L= input("длинна массива?:")
+    A = [0] * Wide
+    for i in range(Wide):
+        A[i] =str(input(" введите элемент массива"))
+    for i in range(N):
+        if len(A[i])<=3:
+            print(A[i], end=" ")
+def tw0_dimensional_array():
+    L= input("длинна массива?:")
+
+    A = [0] * Wide * M
+    for i in range(N):
+        A[i] = str(input(" введите элемент массива"))
+    for i in range(N):
+        if len(A[i]) <= 3:
+            print(A[i], end=" ")
+
+
