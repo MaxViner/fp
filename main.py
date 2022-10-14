2f256a7d5d0d51209182c79fe7671184ef3c0b5f
Max <Max.Vinner@bk.ru>
2022-10-14 23:25:56 +0400

    total correction

--- main.py
+++ main.py
@@ -7,32 +7,36 @@ def one_dimensional_array():
     for i in range(Wide):
         if len(A[i])<=3:
             print(A[i], end=" ")
+
+
 def tw0_dimensional_array():
-    Wide = input("длинна массива?:")
-    Up= input("число строк?:")
-    A = [0] * Wide * Up
-    for i in range(Up):
-        for j in range(Wide):
-            A[i,j] = str(input(" введите элемент массива"))
-        for i in range(Up):
-            for j in range(Wide):
-                if len(A[i,j]) <= 3:
-                    print(A[i,j], end=" ")
+    Wide = int(input("длинна массива?:"))
+    Up = int(input("число строк?:"))
+    A = [[0] * Wide for i in range(Up)]
+    for i in range(0, Up):
+        for i2 in range(0, Wide)
+            if len(A[i][i2]) <= 3:
+                A[i][i2]= str(input())
+
+    for i in range(0, len(A)):
+        for i2 in range(0, len(A[i])):
+            print(A[i][i2], end=' ')
+        print()
 
 def fre_dim_array():
-    Wide = input("длинна массива?:")
-    Up = input("число строк?:")
-    Deep = input("глубина массива?:")
-    A = [0] * Wide * Up * Deep
+    Wide = int(input("длинна массива?:"))
+    Up = int(input("число строк?:"))
+    Deep = int(input("глубина массива?:"))
+    A = [[[0] * Wide for i in range(Up)] for k in range(Deep)]
     for i in range(Up):
         for j in range(Wide):
             for k in range(Deep):
-                A[i, j, k] = str(input(" введите элемент массива"))
+                A[i][ j][ k] = str(input(" введите элемент массива"))
     for i in range(Up):
         for j in range(Wide):
             for k in range(Deep):
-                if len(A[i, j, k]) <= 3:
-                    print(A[i,j,k], end=" ")
+                if len(A[i][j][k]) <= 3:
+                    print( A[i][j][k], end=" ")
 
 def array_selector(N):
 
