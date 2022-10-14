ff452eed4d88e33528491769f9b5fcbffdba2070
Max <Max.Vinner@bk.ru>
2022-10-14 17:10:23 +0400

    create 2_demential_array

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ print("размерность массива?")
 N = int(input())
 print(N)
 def one_dimensional_array():
-    L= input("длинна массива?:")
+    Wide = input("длинна массива?:")
     A = [0] * Wide
     for i in range(Wide):
         A[i] =str(input(" введите элемент массива"))
@@ -11,13 +11,20 @@ def one_dimensional_array():
         if len(A[i])<=3:
             print(A[i], end=" ")
 def tw0_dimensional_array():
-    L= input("длинна массива?:")
+    Wide = input("длинна массива?:")
+    Up= input("число строк?:")
+    A = [0] * Wide * Up
+    for i in range(Up):
+        for j in range(Wide):
+            A[i,j] = str(input(" введите элемент массива"))
+        for i in range(Up):
+            for j in range(Wide):
+                if len(A[i,j]) <= 3:
+                    print(A[i], end=" ")
 
-    A = [0] * Wide * M
-    for i in range(N):
-        A[i] = str(input(" введите элемент массива"))
-    for i in range(N):
-        if len(A[i]) <= 3:
-            print(A[i], end=" ")
+def 3_dim_array():
 
+    Wide = input("длинна массива?:")
+    Up = input("число строк?:")
+    A = [0] * Wide * Up * Deep
 
