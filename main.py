d03254b0858c2edd716c1d3b8b299da785f57194
Max <Max.Vinner@bk.ru>
2022-10-14 17:20:00 +0400

    create 3_dem_array

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 
-print("размерность массива?")
-N = int(input())
-print(N)
 def one_dimensional_array():
-    Wide = input("длинна массива?:")
+    Wide = int(input("длинна массива?:"))
     A = [0] * Wide
     for i in range(Wide):
         A[i] =str(input(" введите элемент массива"))
-    for i in range(N):
+    for i in range(Wide):
         if len(A[i])<=3:
             print(A[i], end=" ")
 def tw0_dimensional_array():
@@ -20,11 +17,34 @@ def tw0_dimensional_array():
         for i in range(Up):
             for j in range(Wide):
                 if len(A[i,j]) <= 3:
-                    print(A[i], end=" ")
+                    print(A[i,j], end=" ")
 
-def 3_dim_array():
-
+def fre_dim_array():
     Wide = input("длинна массива?:")
     Up = input("число строк?:")
+    Deep = input("глубина массива?:")
     A = [0] * Wide * Up * Deep
+    for i in range(Up):
+        for j in range(Wide):
+            for k in range(Deep):
+                A[i, j, k] = str(input(" введите элемент массива"))
+    for i in range(Up):
+        for j in range(Wide):
+            for k in range(Deep):
+                if len(A[i, j, k]) <= 3:
+                    print(A[i,j,k], end=" ")
 
+def array_selector(N):
+
+    if N==1:
+        one_dimensional_array()
+    elif N==2:
+        tw0_dimensional_array()
+    elif N==3:
+        fre_dim_array()
+    else:print("oh...something wrong")
+
+print("размерность массива?")
+N = int(input())
+print(N)
+array_selector(N)
